07d28c58cdba5e457603b501fa672b3356db4351
TANATOS <Kirill.tanatos1993@mail.ru>
2025-01-08 22:05:17 +0300

    fix

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
 
-database_url = 'sqlite+aiosqlite:///db.sqlite3'
+database_url = 'sqlite+aiosqlite:///data/db.sqlite3'
 engine = create_async_engine(url=database_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
 
